b4209f66600442115c18a0e5e01f71f48d2faabe
mhubii <m.huber_1994@hotmail.de>
2020-07-30 17:29:50 +0100

    added asynch spinner to client node

--- src/h_rcom_vs_client_node.cpp
+++ src/h_rcom_vs_client_node.cpp
@@ -3,19 +3,26 @@
 #include <h_rcom_vs/h_rcom_vs_action_client.h>
 
 int main(int argc, char** argv) {
-    ros::init(argc, argv, "bundle_node");
+    ros::init(argc, argv, "h_rcom_vs_client_node");
     ros::NodeHandle nh;
+    auto spinner = ros::AsyncSpinner(2);
+    spinner.start();
 
     std::string action_server, dtwist_topic, p_trocar_topic;
     double dt;
 
-    HRCoMVSActionClient(
+    nh.getParam("action_server", action_server);
+    dtwist_topic = "visual_servo/dtwist";
+    p_trocar_topic = "rcom/p_trocar";
+    nh.getParam("dt", dt);
+
+    HRCoMVSActionClient h_rcom_vs_ac(
         nh,
         action_server, dtwist_topic, p_trocar_topic,
         dt
     );
 
-    ros::spin();
+    ros::waitForShutdown();
     
     return 0;
 }
