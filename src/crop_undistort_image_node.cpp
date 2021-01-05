b9f38dee8bf24b8c857811c3b050235594c79fd4
mhubii <m.huber_1994@hotmail.de>
2021-01-05 23:04:12 +0000

    merged crop and undistort into single node

--- src/crop_undistort_image_node.cpp
+++ src/crop_undistort_image_node.cpp
@@ -3,38 +3,39 @@
 #include <image_transport/image_transport.h>
 #include <image_geometry/pinhole_camera_model.h>
 
+// we implement this node as image_proc as poor support for rectifying a cropped image,
+// which often leads to errors
 // see also http://docs.ros.org/en/jade/api/image_proc/html/rectify_8cpp_source.html
 
 image_transport::CameraSubscriber sub;
 image_transport::Publisher pub;
 image_geometry::PinholeCameraModel model;
 
+int x_offset, y_offtset, width, height;
+
 void undistortCallback(const sensor_msgs::ImageConstPtr& img_msg, const sensor_msgs::CameraInfoConstPtr& info_msg) {
     
     model.fromCameraInfo(info_msg);
 
-    cv_bridge::CvImagePtr cv_ptr;
-    try
-    {
-        cv_ptr = cv_bridge::toCvCopy(img_msg, sensor_msgs::image_encodings::YUV422); // copy image
-    }
-    catch(const std::exception& e)
-    {
-        ROS_ERROR("cv_bridge exception: %s", e.what());
-        return;
-    }
+    auto img = cv_bridge::toCvShare(img_msg)->image;
 
     cv::Mat rect;
+    cv::Rect roi(x_offset, y_offtset, width, height);
 
-    model.rectifyImage(cv_ptr->image, rect);
+    model.rectifyImage(img(roi), rect);
 
     sensor_msgs::ImagePtr rect_msg = cv_bridge::CvImage(img_msg->header, img_msg->encoding, rect).toImageMsg();
     pub.publish(rect_msg);
 };
 
 int main(int argc, char** argv) {
-    ros::init(argc, argv, "undistort_image_node");
+    ros::init(argc, argv, "crop_undistort_image_node");
     ros::NodeHandle nh;
+
+    nh.getParam("crop_undistort_image_node/x_offset", x_offset);
+    nh.getParam("crop_undistort_image_node/y_offtset", y_offtset);
+    nh.getParam("crop_undistort_image_node/width", width);
+    nh.getParam("crop_undistort_image_node/height", height);
 
     image_transport::ImageTransport it(nh);
     sub = it.subscribeCamera("image", 1, undistortCallback);
